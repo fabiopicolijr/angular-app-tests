abad0d26e2109fbf909f0e6de4d3db42a3e7e161
Fábio Picoli <fabiopicolijr@gmail.com>
2023-02-21 15:29:40 -0300

    studying
    COMPONENTS
     - CREATE_WITH_CLI
     - INLINE_COMPONENT
    STYLES
    - INLINE_STYLE
    COMPOMENT_SELECTOR_FULL_UNDERSTANDING
    DATABINDING
    - string interpolation
    - property binding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { ServerComponent } from './server/server.component';
-import { ServersComponent } from './servers/servers.component';
+// import { successAlertComponent } from './section2/challenge1/success-alert/success-alert.component';
+// import { WarningAlertComponent } from './section2/challenge1/warning-alert/warning-alert.component';
+import { ServerComponent } from './section1/server/server.component';
+import { ServersComponent } from './section1/servers/servers.component';
 
 @NgModule({
-  declarations: [AppComponent, ServerComponent, ServersComponent],
+  declarations: [AppComponent, ServersComponent, ServerComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
