0ee9ae0abb58fcd944bd1d0b8ddc2d46acc5ecbc
Fábio Picoli <fabiopicolijr@gmail.com>
2023-05-10 19:49:32 -0300

    changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,10 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
 import { ServersComponent } from './servers/servers.component';
-import { DatabindingComponent } from './section2/challenge2/databinding/databinding.component';
 import { Challenge3Component } from './challenge3/challenge3.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    ServersComponent,
-    ServerComponent,
-    DatabindingComponent,
-    Challenge3Component,
-  ],
+  declarations: [AppComponent, ServersComponent, ServerComponent],
   imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
